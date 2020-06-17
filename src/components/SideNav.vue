da57dfad7c8ec736837974a65e34bed460c4bf02
Hidemitsu Aoki <olympos0117@gmail.com>
2020-06-17 10:06:25 +0900

    Add link to SideNav

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,37 +1,38 @@
 <template>
-    <v-navigation-drawer v-model="$store.state.drawer" absolute temporary>
-      <v-list>
-        <v-list-item>
-          <v-list-item-avatar>
-            <img
-              src="https://j-town.net/images/2019/town/town20191010173054.jpg"
-            />
-          </v-list-item-avatar>
-          <v-list-item-content>
-            <v-list-item-title>Hidemitsu Aoki</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+  <v-navigation-drawer v-model="$store.state.drawer" absolute temporary>
+    <v-list>
+      <v-list-item>
+        <v-list-item-avatar>
+          <img src="https://j-town.net/images/2019/town/town20191010173054.jpg" />
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title>Hidemitsu Aoki</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
 
-        <v-divider></v-divider>
+      <v-divider></v-divider>
 
-        <v-list-item v-for="(item, index) in items" :key="index">
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+      <v-list-item v-for="(item, index) in items" :key="index" :to="item.link">
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      items: [{ title: "連絡先一覧", icon: "mdi-menu" }],
+      items: [
+        { title: "ホーム", icon: "mdi-home", link: { name: "home" } },
+        { title: "連絡先一覧", icon: "mdi-menu", link: { name: "addresses" } }
+      ]
     };
-  },
+  }
 };
 </script>
