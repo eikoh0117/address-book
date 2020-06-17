7ce8d66a08f1602b8fb62a13651b140a9019d93b
Hidemitsu Aoki <olympos0117@gmail.com>
2020-06-17 22:03:39 +0900

    Make it possible to update addresses

--- src/store/index.js
+++ src/store/index.js
@@ -24,6 +24,10 @@ export default new Vuex.Store({
       address.id = id;
       state.addresses.push(address);
     },
+    updateAddress(state, { id, address }) {
+      const index = state.addresses.findIndex((address) => address.id === id);
+      state.addresses[index] = address;
+    },
   },
   actions: {
     setLoginUser({ commit }, user) {
@@ -61,6 +65,18 @@ export default new Vuex.Store({
           .add(address)
           .then((doc) => {
             commit("addAddress", { id: doc.id, address });
+          });
+      }
+    },
+    updateAddress({ getters, commit }, { id, address }) {
+      if (getters.uid) {
+        firebase
+          .firestore()
+          .collection(`users/${getters.uid}/addresses`)
+          .doc(id)
+          .update(address)
+          .then(() => {
+            commit(("updateAddress", { id, address }));
           });
       }
     },
